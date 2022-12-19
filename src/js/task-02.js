c35c26b38b846d9a55db5be6abf6f6989d74c096
YuliiaRiabchuk <yuliiariabchuk@gmail.com>
2022-12-19 23:26:28 +0200

    add first task

---
+++ src/js/task-02.js
@@ -0,0 +1,22 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients.map((element) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = element;
+
+  return li;
+});
+
+console.log(elements);
+
+list.append(...elements);
